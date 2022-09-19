da8df2215533fe898c86ecee5aab60bafee07e3f
Abhishek Kaushik <Abhishek.nexus26@gmail.com>
2022-09-19 05:46:39 +0530

    Scaffold create-game message

--- ts-client/zireael26.checkers.checkers/registry.ts
+++ ts-client/zireael26.checkers.checkers/registry.ts
@@ -1,6 +1,8 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
+import { MsgCreateGame } from "./types/checkers/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
+    ["/zireael26.checkers.checkers.MsgCreateGame", MsgCreateGame],
     
 ];
 
