567280360936acf07523121ea7b134394887cd40
Abhishek Kaushik <Abhishek.nexus26@gmail.com>
2022-09-21 01:21:28 +0530

    Added Play Move message, its implementation and unit tests

--- ts-client/zireael26.checkers.checkers/registry.ts
+++ ts-client/zireael26.checkers.checkers/registry.ts
@@ -1,8 +1,10 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
 import { MsgCreateGame } from "./types/checkers/tx";
+import { MsgPlayMove } from "./types/checkers/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
     ["/zireael26.checkers.checkers.MsgCreateGame", MsgCreateGame],
+    ["/zireael26.checkers.checkers.MsgPlayMove", MsgPlayMove],
     
 ];
 
