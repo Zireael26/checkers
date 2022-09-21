83276bfb538a3e475b8c4957a056da34084cce71
Abhishek Kaushik <Abhishek.nexus26@gmail.com>
2022-09-22 00:37:38 +0530

    Added Events upon creating a game and playign a move and their unit tests

--- vue/src/store/generated/zireael26.checkers.checkers/index.ts
+++ vue/src/store/generated/zireael26.checkers.checkers/index.ts
@@ -223,19 +223,6 @@ export default {
 		},
 		
 		
-		async sendMsgCreateGame({ rootGetters }, { value, fee = [], memo = '' }) {
-			try {
-				const client=await initClient(rootGetters)
-				const result = await client.Zireael26CheckersCheckers.tx.sendMsgCreateGame({ value, fee: {amount: fee, gas: "200000"}, memo })
-				return result
-			} catch (e) {
-				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgCreateGame:Init Could not initialize signing client. Wallet is required.')
-				}else{
-					throw new Error('TxClient:MsgCreateGame:Send Could not broadcast Tx: '+ e.message)
-				}
-			}
-		},
 		async sendMsgPlayMove({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const client=await initClient(rootGetters)
@@ -249,20 +236,20 @@ export default {
 				}
 			}
 		},
-		
-		async MsgCreateGame({ rootGetters }, { value }) {
+		async sendMsgCreateGame({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
-				const client=initClient(rootGetters)
-				const msg = await client.Zireael26CheckersCheckers.tx.msgCreateGame({value})
-				return msg
+				const client=await initClient(rootGetters)
+				const result = await client.Zireael26CheckersCheckers.tx.sendMsgCreateGame({ value, fee: {amount: fee, gas: "200000"}, memo })
+				return result
 			} catch (e) {
 				if (e == MissingWalletError) {
 					throw new Error('TxClient:MsgCreateGame:Init Could not initialize signing client. Wallet is required.')
-				} else{
-					throw new Error('TxClient:MsgCreateGame:Create Could not create message: ' + e.message)
+				}else{
+					throw new Error('TxClient:MsgCreateGame:Send Could not broadcast Tx: '+ e.message)
 				}
 			}
 		},
+		
 		async MsgPlayMove({ rootGetters }, { value }) {
 			try {
 				const client=initClient(rootGetters)
@@ -273,6 +260,19 @@ export default {
 					throw new Error('TxClient:MsgPlayMove:Init Could not initialize signing client. Wallet is required.')
 				} else{
 					throw new Error('TxClient:MsgPlayMove:Create Could not create message: ' + e.message)
+				}
+			}
+		},
+		async MsgCreateGame({ rootGetters }, { value }) {
+			try {
+				const client=initClient(rootGetters)
+				const msg = await client.Zireael26CheckersCheckers.tx.msgCreateGame({value})
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgCreateGame:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgCreateGame:Create Could not create message: ' + e.message)
 				}
 			}
 		},
